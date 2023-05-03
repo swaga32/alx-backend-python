02544f61b60a1e009d353abf079e3f758a2d9062
swaga32 <jacksonmbirui3@gmail.com>
2023-05-03 18:53:52 +0000

    kik

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,8 +7,8 @@ from typing import Generator
 # Generator[yield_type, send_type, return_type]
 
 async def async_generator() -> Generator[float, None, None]:
-    '''Coroutine that generates a sequence of 5 numbers.
+    '''Coroutine that generates a sequence of 20 numbers.
     '''
-    for _ in range(5):
+    for _ in range(20):
         await asyncio.sleep(1)
-        yield random.random() * 5
+        yield random.random() * 20
