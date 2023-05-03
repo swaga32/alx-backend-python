6f2cee98e7d4f76b53cf2c53be7334ce85bc6406
swaga32 <jacksonmbirui3@gmail.com>
2023-05-03 19:05:30 +0000

    tree

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Task 4
+'''
+import asyncio
+import time
+from importlib import import_module as find
+
+async_comprehension = find('1-async_comprehension').async_comprehension
+
+async def measure_runtime() -> float:
+    '''Executes async_comprehension 4 times and measures the
+    total execution time.
+    '''
+    start_time = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time.perf_counter()
+    return end_time - start_time
